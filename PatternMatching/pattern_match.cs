a8e1609b83a99f5467e25cd60ecfdba927d25c31
isrt09 <mazedur@gmail.com>
2024-07-08 11:24:46 +0600

    add Traditional typecast

--- PatternMatching/pattern_match.cs
+++ PatternMatching/pattern_match.cs
@@ -7,10 +7,31 @@ namespace PatternMatching
         public string? CustomerContact { get; set; }
     }
 
-    class Product : Customer
+    public class Product : Customer
     {
         public int ProductID { get; set; }
         public double ProductPrice { get; set; }
         public int ProductOrderNo { get; set; }
+    }
+
+    public class Program
+    {
+         // Main Method
+         static void Main(string[] args)
+        {
+            Customer customer;
+            customer = new Product()
+            {
+                CustomerName = "James Smith",
+                CustomerID = 1001,
+                CustomerContact = "01916178600",
+                ProductID = 10012,
+                ProductPrice = 120.45,
+                ProductOrderNo = 5
+            };
+            // Traditional way typecast e.g. Downcasting or Upcasting
+            Product product = (Product) customer; 
+            Console.WriteLine(product.CustomerName);
+        }
     }
 }
