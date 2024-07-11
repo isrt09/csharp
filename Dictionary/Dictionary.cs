901edf5ccbd2f182de0ce66b16972caebd142407
isrt09 <mazedur@gmail.com>
2024-07-11 21:19:51 +0600

    update dictionary key, values

--- Dictionary/Dictionary.cs
+++ Dictionary/Dictionary.cs
@@ -16,6 +16,9 @@ namespace DictionaryCollections
 
             // Add or Insert more key, values
             products.Add(104,"Laptop Dell Inspiron N1344");
+
+            // Update key, values
+            products[104] = "Walton 27 Inch FHD Monitor";
             
             // print dictonary values
             foreach (KeyValuePair<int,string> product in products)            
