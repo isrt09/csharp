17c2c095f169b5fe25eec4c234437ab578c6d6eb
isrt09 <mazedur@gmail.com>
2024-07-13 11:01:46 +0600

    initialize product objects

--- Collections Object/Program001.cs
+++ Collections Object/Program001.cs
@@ -15,5 +15,16 @@ namespace ObjectCollections
             this.ProductPrice = productPrice;
             this.OrderDate = orderDate;
         }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create some products objects
+            Product product1 = new Product(101,"Dell Laptop", 125.25, DateTime.Now);
+            Product product2 = new Product(102,"Sumsung Laptop", 175.45, DateTime.Now);
+            Product product3 = new Product(103,"Ausus Laptop", 325.26, DateTime.Now);            
+        }
     }    
 }
