3dc9976e9f9c2ba5cd004ff217ae46f99be33b13
isrt09 <mazedur@gmail.com>
2024-07-14 23:56:06 +0600

    add custom collection class

--- IEnumerable/Program.cs
+++ IEnumerable/Program.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace IEnumerable
 {
     // create customer
@@ -5,6 +7,23 @@ namespace IEnumerable
     {
         public int CustomerID { get; set; }
         public string? CustomerName { get; set; }        
+    }
+
+    //custom collection class
+    public class CustomerLists : IEnumerable
+    {
+        private List<Customer> customers = new List<Customer>();
+        public IEnumerator GetEnumerator()
+        {
+            for(int i=0; i< customers.Count; i++)
+            {
+                yield return customers[i];
+            }
+        }
+        public void Add(Customer customer)        
+        {            
+            customers.Add(customer);
+        }
     }
 
     // create product 
